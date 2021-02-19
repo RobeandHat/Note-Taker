c4b37334e0090e770f6241147bedc394ddf97e20
Matt Randolph <mttrndlph@gmail.com>
2021-02-19 16:34:57 -0500

    Added server js. Added routes, dependencies, listener etc.

---
+++ server.js
@@ -0,0 +1,26 @@
+//Dependencies
+
+const express = require("express");
+const path = require("path");
+
+//Sets up express app
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//sets up express app to handle parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+//Server listening
+
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
