7f9a2b2bd549c7093719974265a20f97461c5e90
Matt Randolph <mttrndlph@gmail.com>
2021-02-19 19:23:17 -0500

    added static route to server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 //sets up express app to handle parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //Routes
 app.get("/", (req, res) =>
